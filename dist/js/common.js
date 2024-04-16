44b0a8cad91adc9ced59e4be81c31e0347714bb2
Denis <webpassion9@gmail.com>
2024-04-16 21:31:56 +0300

    Initial commit

---
+++ dist/js/common.js
@@ -0,0 +1,26 @@
+// import * as flsFunctions from "./modules/functions.js";
+// import Swiper, { Navigation, Pagination } from 'swiper';
+// import 'swiper/css/bundle';
+
+// flsFunctions.isWebp();
+
+// --------------------------------------------------------------------------
+// correct ratio
+// --------------------------------------------------------------------------
+// function forcedOriginalScale(containerClass) {
+//   var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+  
+//   if (!isMobile) {
+//     var App = document.querySelector(containerClass);
+//     App.style.zoom = 1 / devicePixelRatio;
+//   }
+// }
+
+// document.addEventListener(
+//   "DOMContentLoaded",
+//   function() {
+//     forcedOriginalScale('.wrapper');
+//   }
+// );
+
+
